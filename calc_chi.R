55d774ab1b9458acc098b9850b68c1529a7711ff
eaperkowski <evan.a.perkowski@ttu.edu>
2022-07-19 17:45:46 -0500

    update calc_chi to have separate c3 and c4 forms, rather than integrated into one fxn

--- calc_chi.R
+++ calc_chi.R
@@ -1,41 +1,34 @@
-calc_chi <- function(leaf.d13c, type = c("c3", "c4", NA)) {
+calc_chi_c3 <- function(leaf.d13c) {
   
   # Global constants
   air = -8
   a = 4.4
   b = 27
-  c = -5.7
-  d = 30
-  phi = 0.4
   
   # Leaf carbon discrimination relative to air
   delta <- (air - leaf.d13c) / (1 + leaf.d13c * 0.001)
-  
-  if(is.na(type) == TRUE) {
-    type == "c3"
-    warning("No photosynthetic pathway input. Defaulting to c3 pathway")
-    
-  }
-  
-  if(type == "c3") {
-    
   chi <- (delta - a) / (b - a)
   
-  }
+  return(chi)
   
-  if(type == "c4") {
-    b.c4 <- c + (d * phi)
-    chi <- (delta - a) / (b.c4 - a)
+}
 
-  }
+
+calc_chi_c4 <- function(leaf.d13c) {
+  # Global constants
+  air = -8
+  a = 4.4
+  c = -5.7
+  d = 30
+  phi = 0.37
   
-  if(type != "c3" & type != "c4") {
-    
-    chi <- (delta - a) / (b - a)
-    warning("Invalid photosynthetic pathway. Defaulting to c3 pathway")
-    
-  }
+  b.c4 = -5.7 + (d * phi)
+  
+  # Leaf carbon discrimination relative to air
+  delta <- (air - leaf.d13c) / (1 + leaf.d13c * 0.001)
+  chi <- (delta - a) / (b.c4 - a)
   
   return(chi)
-
+  
 }
+
