a2e02c55a8a01b6734e4a2853d65d65e83ec8ee7
eaperkowski <evan.a.perkowski@ttu.edu>
2022-08-25 16:33:12 -0500

    add proportion of N in leaf tissue fxn; modify stomLim to no longer need elevation correction

---
+++ propN_funcs.R
@@ -0,0 +1,142 @@
+###############################################################################
+# p_rubisco(vcmax25, narea):
+###############################################################################
+#
+# Calculates proportion of leaf nitrogen in Rubisco following equations from
+# Niinemets et al. (1997) and Niinements et al. (1998)
+#
+# Function arguments:
+#   - vcmax25     = maximum Rubisco carboxylation rate, standardized to 25degC
+#                  (μmol m^-2 s^-1)
+#   - narea       = leaf nitrogen per leaf area (gN m^-2)
+#
+# Returns:
+# Vector with proportion of leaf N to Rubisco (p_rubisco; gN Rubisco gN^-1)
+p_rubisco <- function(vcmax25, narea){
+  
+  vcr = 20.5 #umol CO2 (g Rubisco)-1 s-1 at 25C
+  # vcmax25 in umol m-2 s-1
+  # marea is g m-2
+  # nmass is gN g-1
+  # 6.25 converts nitrogen to protein (rubisco)
+  
+  p_rubisco = vcmax25 / (vcr * narea * 6.25)
+  
+  p_rubisco # g N Rubisco / g N
+  
+}
+
+###############################################################################
+# p_bioenergetics(jmax25, narea):
+###############################################################################
+#
+# Calculates proportion of leaf nitrogen allocated to bioenergetics. Function
+# follows equations from Niinemets et al. (1997) and Niinements et al. (1998)
+#
+# Function arguments:
+#   - jmax25     = maximum RuBP regeneration rate, standardized to 25degC
+#                  (μmol m^-2 s^-1)
+#   - narea      = leaf nitrogen per leaf area (gN m^-2)
+#
+#
+# Returns:
+# Vector with proportion of leaf N to bioenergetics (p_bioenergetics; 
+# g N bioenergetics g N^-1)
+p_bioenergetics <- function(jmax25, narea){
+  
+  jmc = 156 # capacity of electron transport per unit of cytochrome f (umol electrons (umol cyt f)-1 s-1)
+  # jmax25 in umol m-2 s-1
+  # marea is g m-2
+  # nmass is gN g-1
+  # 8.06 converts nitrogen to protein (cytf)
+  
+  p_bioenergetics = jmax25 / (jmc * narea * 8.06)
+  
+  p_bioenergetics # g N bioenergetics / g N
+  
+}
+
+###############################################################################
+# p_lightharvesting(chlorophyll, nmass):
+###############################################################################
+#
+# Calculates proportion of leaf nitrogen allocated to light harvesting. Function
+# follows equations from Niinemets et al. (1997) and Niinements et al. (1998)
+#
+# Function arguments:
+#   - chlorophyll = chlorophyll content (mmol g^-1)
+#   - nmass       = leaf nitrogen per leaf mass (g N g^-1)
+#
+#
+# Returns:
+# Vector with proportion of leaf N to light harvesting (p_lightharvesting; 
+# g N light harvesting g N^-1)
+p_lightharvesting <- function(chlorophyll, nmass){
+  
+  # chlorophyll content in mmol g-1
+  # nmass in gN g-1
+  # marea in g m-2
+  chlorophyll_mol = chlorophyll / 1000
+  cb = 2.75 / 1000 # chlorophyll binding in mol chlorophyll (g N chlorophyll)-1 assuming most is in LHCII
+  
+  p_lightharvesting = chlorophyll_mol *  (1 / nmass) * (1 / cb)
+  
+  p_lightharvesting # g N light harvesting / g N
+  
+  # cytf_m2 = (jmax25 / 156) / 1000 # mmol cyt f m-2
+  # cytf_chlor = cytf_m2 * (1/marea) * (1/chlorophyll_mol) # mmol cyt f (mol chlorophyll)-1
+  # psii_chlor = 1.98 * cytf_chlor - (0.365 * (cytf_chlor^2)) # mmol (mol chlorophyll)-1
+  # psi_chlor = 1.7 # mmol (mol chlorophyll)-1
+  # psii_g = psii_chlor * (chlorophyll / 1000) # mmol g-1
+  # psi_g = psi_chlor * (chlorophyll / 1000) # mmol g-1
+  # 
+  # lhcii_g = chlorophyll - (psii_g + psi_g)
+  # 
+  # psii_prop = psii_g / chlorophyll
+  # psi_prop = psi_g / chlorophyll
+  # lhcii_prop = lhcii_g / chlorophyll
+  # 
+  # cb_psii = 0.858 # mmol chl (gN)-1
+  # cb_psi = 2.18 # mmol chl (gN)-1
+  # cb_lhcii = 2.75 # mmol chl (gN)-1
+  # 
+  # p_lightharvesting = (chlorophyll / nmass) * (1/((psii_prop*cb_psii) + (psi_prop*cb_psi) +(lhcii_prop*cb_lhcii)))
+}
+
+###############################################################################
+# p_lightharvesting(chlorophyll, nmass):
+###############################################################################
+#
+# Calculates proportion of leaf nitrogen allocated to structure. Function uses
+# LMA as an input per eqs. listed in Dong et al. (2017) Biogeosciences
+#
+# Function arguments:
+#   - chlorophyll = chlorophyll content (mmol g^-1)
+#   - nmass       = leaf nitrogen per leaf mass (g N g^-1)
+#
+#
+# Returns:
+# Vector with proportion of leaf N to light harvesting (p_lightharvesting; 
+# g N light harvesting g N^-1)
+### lma conversion from Dong et al. (2017) Biogeosciences
+p_structure = function(lma, narea){
+  
+  nstructure = ((10^-2.67) * (lma ^ 0.99)) / narea
+  nstructure
+  
+}
+###############################################################################
+# References
+###############################################################################
+
+# Niinemets Ü., Kull O. & Tenhunen J.D. (1998) An analysis of light effects on 
+# foliar morphology, physiology, and light interception in temperate deciduous 
+# woody species of contrasting shade tolerance. Tree Physiology 18, 681–696.
+
+# Niinemets Ü. & Tenhunen J.D. (1997) A model separating leaf structural and 
+# physiological effects on carbon gain along light gradients for the 
+# shade-tolerant species Acer saccharum. Plant, Cell & Environment 20, 845–866.
+
+# Dong N., Prentice I.C., Evans B.J., Caddy-Retalic S., Lowe A.J. & Wright I.J. 
+# (2017) Leaf nitrogen from first principles: field evidence for adaptive 
+# variation with climate. Biogeosciences 14, 481–495.
--- stomatal_limitation.R
+++ stomatal_limitation.R
@@ -10,25 +10,44 @@
 #    - A_net        =  Net photosynthesis (A) at 400ppm CO2
 #    - Vcmax        =  Unstandardized Vcmax measurement (because A_net is 
 #                      not standardized) 
-#    - elevation    =  elevation by which A_net and Vcmax were measured. Required
-#                      to calculate Γ* and Km
-#    - temp         =  temperature by which A_net and Vcmax were measured. Required
-#                      to calculate Γ* and Km
+#    - temp         =  leaf temperature by which A_net and Vcmax were measured.
+#                      Required to calculate Γ* and Km
+#    - Rd.meas      =  Default FALSE. If false, calculates as function of Vcmax
+#    - Rd           =  measured dark respiration. Rd.meas should be set to TRUE
+#                      if Rd data are included
+#    - K            =  Boolean noting whether temperature is in K or not. Default
+#                      is FALSE (noting temperature is not in K)
 #
 # Returns:
-#    - Data frame containing Michaelis-Menton constant (K), gammastar, A_mod,
-#      and stomatal limitation value (stomLim).
+#    - Data frame containing Michaelis-Menton constants (Kc, Ko, Km), gammastar,
+#      A_mod, and stomatal limitation value (stomLim).
 # 
 # Note: Stomatal limitation should be a value less than or equal to one
-stomatal_limitation <- function(A_net, Vcmax, elevation, leaf.temp, 
-                                Rd.meas = FALSE, Rd) {
-   require(rpmodel)
-   
-   
-   # Global constants
-   p.atm = patm(elv = elevation)
-   K = kmm(tc = leaf.temp, patm = p.atm)
-   gamma.star = gammastar(tc = leaf.temp, patm = p.atm)
+stomatal_limitation <- function(A_net, Vcmax, leaf.temp, 
+                                Rd.meas = FALSE, Rd, temp = c("C", "K")) {
+  
+  ## Global constants
+  R = 8.314 # universal gas constant (J mol^-1 K^-1)
+  Oi = 210 # leaf intercellular O2 concentration (mmol mol^-1)
+  
+  ## Leaf temperature correction
+  leaf.temp <- ifelse(temp == "C", 
+                      leaf.temp + 273.15, 
+                      ifelse(temp == "K",
+                             leaf.temp,
+                             NA))
+
+  ## Calculate Kc
+  Kc <- 404.9*exp((79430*(leaf.temp - 298))/(298*R*leaf.temp))
+
+  ## Calculate Ko
+  Ko <- 278.4*exp((36380*(leaf.temp - 298))/(298*R*leaf.temp))
+  
+  ## Calculate Km
+  Km <- Kc * (1 + (Oi / Ko))
+  
+  ## Calculate gamma.star
+  gammastar <- 42.75*exp((37830*(leaf.temp - 298))/(298*R*leaf.temp))
    
    # Add contingency over whether Rd was measured or not
    if(Rd.meas == FALSE) {
@@ -40,12 +59,12 @@ stomatal_limitation <- function(A_net, Vcmax, elevation, leaf.temp,
    }
 
    # Calculate A_mod
-   A_mod = Vcmax * ((400 - gamma.star) / (400 + K)) - Rd
+   A_mod = Vcmax * ((400 - gammastar) / (400 + Km)) - Rd
    
    # Determine stomatal limitation
    l <- 1 - (A_net / A_mod)
    
-   return(data.frame("l" = l,
-               "gamma.star" = gamma.star, 
-               "Km" = K))
+   return(data.frame(Kc, Ko, Km, gammastar, l))
 }
+
+
