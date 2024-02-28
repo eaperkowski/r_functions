3dfe02b5a9ca4bd89f18369d79c281fe19db4f23
eaperkowski <evan.a.perkowski@ttu.edu>
2024-02-28 16:35:57 -0600

    add method for calculating percent ndfa using atmospheric %

--- calc_ndfa.R
+++ calc_ndfa.R
@@ -11,12 +11,23 @@
 #
 # Returns:
 #     - Vector containing %Ndfa. Vector is already in percent form
-calc_ndfa <- function(ref.15n = NA, sample.15n = NA, B = NA) {
-
-  ndfa <- (ref.15n - sample.15n) / (ref.15n - B) * 100 
+calc_ndfa <- function(ref.15n = NA, sample.15n = NA, B = NA, use_atm_val = TRUE) {
   
-  return(ndfa)
+  if(use_atm_val == TRUE) {
+    
+    d15n_atm <- 0.3663 # % of 15N in atmosphere (Dawson et al., 2002)
+    
+    ndfa <- (ref.15n - sample.15n) / (ref.15n - d15n_atm) * 100
+  }
+  
+  else(ndfa <- (ref.15n - sample.15n) / (ref.15n - B) * 100)
+  
+  return(data.frame(ndfa))
 }
 
 # Test
-#calc_ndfa(ref.15n = 1, sample.15n = 0.5, B = 0)
+#calc_ndfa(ref.15n = 0, sample.15n = 0.5, B = 1, use_atm_val = TRUE)
+#calc_ndfa(ref.15n = 0, sample.15n = 0.5, B = 0, use_atm_val = TRUE)
+
+# alc_ndfa(ref.15n = 0, sample.15n = 0.5, B = 1, use_atm_val = FALSE)
+#calc_ndfa(ref.15n = 0, sample.15n = 0.1, B = 0.5, use_atm_val = FALSE)
