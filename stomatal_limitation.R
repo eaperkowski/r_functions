9233acf027b881e2bbb4f4317eb0fa1e3b127b81
eaperkowski <evan.a.perkowski@ttu.edu>
2024-06-05 10:34:07 -0400

    correct stomatal limitation function to allow flexible Ci=Ca for Amod calc

--- stomatal_limitation.R
+++ stomatal_limitation.R
@@ -7,24 +7,30 @@
 #
 #
 # Arguments:
-#    - A_net        =  Net photosynthesis (A) at 400ppm CO2
-#    - Vcmax        =  Unstandardized Vcmax measurement (because A_net is 
-#                      not standardized) 
-#    - temp         =  leaf temperature by which A_net and Vcmax were measured.
-#                      Required to calculate Γ* and Km
-#    - Rd.meas      =  Default FALSE. If false, calculates as function of Vcmax
-#    - Rd           =  measured dark respiration. Rd.meas should be set to TRUE
-#                      if Rd data are included
-#    - K            =  Boolean noting whether temperature is in K or not. Default
-#                      is FALSE (noting temperature is not in K)
+#    - Anet        =    Net photosynthesis (A)
+#    - Vcmax        =   Unstandardized Vcmax measurement (because Anet is 
+#                       not standardized) 
+#    - leaf.temp    =   leaf temperature by which Anet and Vcmax were measured.
+#                       Required to calculate Γ* and Km
+#    - Anet_ca      =   Atmospheric CO2 level used for Anet (default: 420ppm)
+#    - Rd.meas      =   Calculates as function of Vcmax (default: FALSE)
+#    - Rd           =   measured dark respiration. Rd.meas should be set to TRUE
+#                       if Rd data are included
+#    - K            =   Boolean noting whether temperature is in K or not 
+#                       (default: FALSE)
 #
 # Returns:
 #    - Data frame containing Michaelis-Menton constants (Kc, Ko, Km), gammastar,
 #      A_mod, and stomatal limitation value (stomLim).
 # 
 # Note: Stomatal limitation should be a value less than or equal to one
-stomatal_limitation <- function(A_net, Vcmax, leaf.temp, 
-                                Rd.meas = FALSE, Rd, temp = c("C", "K")) {
+stomatal_limitation <- function(Anet, 
+                                Vcmax, 
+                                leaf.temp, 
+                                Anet_ca = 420,
+                                Rd.meas = FALSE, 
+                                Rd, 
+                                temp = c("C", "K")) {
   
   ## Global constants
   R = 8.314 # universal gas constant (J mol^-1 K^-1)
@@ -38,16 +44,19 @@ stomatal_limitation <- function(A_net, Vcmax, leaf.temp,
                              NA))
 
   ## Calculate Kc
-  Kc <- 404.9*exp((79430*(leaf.temp - 298))/(298*R*leaf.temp))
+  Kc <- 404.9 * exp((79430 * (leaf.temp - 298)) / 
+                      (298 * R * leaf.temp))
 
   ## Calculate Ko
-  Ko <- 278.4*exp((36380*(leaf.temp - 298))/(298*R*leaf.temp))
+  Ko <- 278.4 * exp((36380 * (leaf.temp - 298)) / 
+                      (298 * R * leaf.temp))
   
   ## Calculate Km
   Km <- Kc * (1 + (Oi / Ko))
   
   ## Calculate gamma.star
-  gammastar <- 42.75*exp((37830*(leaf.temp - 298))/(298*R*leaf.temp))
+  gammastar <- 42.75 * exp((37830 * (leaf.temp - 298)) / 
+                             (298 * R * leaf.temp))
    
    # Add contingency over whether Rd was measured or not
    if(Rd.meas == FALSE) {
@@ -59,10 +68,10 @@ stomatal_limitation <- function(A_net, Vcmax, leaf.temp,
    }
 
    # Calculate A_mod
-   A_mod = Vcmax * ((400 - gammastar) / (400 + Km)) - Rd
+   Amod = Vcmax * ((Anet_ca - gammastar) / (Anet_ca + Km)) - Rd
    
    # Determine stomatal limitation
-   l <- 1 - (A_net / A_mod)
+   l <- 1 - (Anet / Amod)
    
    return(data.frame(Kc, Ko, Km, gammastar, l))
 }
